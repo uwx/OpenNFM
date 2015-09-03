dd3f63de49684e389c5a3a011b7e99da92aa5969
chrishansen69 <chris331hansen@outlook.com>
2015-09-03 00:18:13 -0300

    fail

--- RadicalMidi.java
+++ RadicalMidi.java
@@ -10,36 +10,68 @@ import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.Synthesizer;
 
+import java.io.IOException;
+ 
+// mp3 ogg imports
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine.Info;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
+import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+
 public class RadicalMidi {
 
     InputStream is;
     Sequencer sequencer;
     boolean loaded = false;
     boolean playing = false;
+    boolean isMp3Ogg = false;
     String s;
     FileInputStream fi;
     
     public RadicalMidi(String fn)
     {
-    	s = fn;
-    	try {
-    	fi = new FileInputStream(new File(fn));
-    	} catch(java.io.FileNotFoundException ex) {
-    		System.out.println("Midi file not found!");
-    		ex.printStackTrace();
+    	if (fn.endsWith(".ogg") || fn.endsWith(".mp3")) {
+    		isMp3Ogg = true;
+    	} else {
+    		isMp3Ogg = false;
+	    	s = fn;
+	    	try {
+	    	fi = new FileInputStream(new File(fn));
+	    	} catch(java.io.FileNotFoundException ex) {
+	    		System.out.println("Midi file not found!");
+	    		ex.printStackTrace();
+	    	}
+	    	try {
+	    	// Obtains the default Sequencer connected to a default device.
+	    	sequencer = MidiSystem.getSequencer();
+	
+	    	// Opens the device, indicating that it should now acquire any
+	    	// system resources it requires and become operational.
+	    	sequencer.open();
+	
+	    	} catch (Exception ex) {
+	    		System.out.println("Error loading Midi file:");
+	    		ex.printStackTrace();
+	    	}
     	}
-    	try {
-    	// Obtains the default Sequencer connected to a default device.
-    	sequencer = MidiSystem.getSequencer();
-
-    	// Opens the device, indicating that it should now acquire any
-    	// system resources it requires and become operational.
-    	sequencer.open();
-
-    	} catch (Exception ex) {
-    		System.out.println("Error loading Midi file:");
-    		ex.printStackTrace();
-    	}
+    }
+    
+    public void load() {
+    	if (!isMp3Ogg)
+    		loadMidi();
+    }
+    
+    public void play() {
+    	if (isMp3Ogg)
+    		playMp3Ogg(s);
+    	else
+    		playMidi();
     }
     
     public void loadMidi()
@@ -228,4 +260,52 @@ public class RadicalMidi {
 			ex.printStackTrace();
 		}
 	}
+	
+    public void playMp3Ogg(String filePath) {
+        final File file = new File(filePath);
+ 
+        try (final AudioInputStream in = getAudioInputStream(file)) {
+             
+            final AudioFormat outFormat = getOutFormat(in.getFormat());
+            final Info info = new Info(SourceDataLine.class, outFormat);
+            final SourceDataLine line;
+            
+            try {
+            	line = (SourceDataLine) AudioSystem.getLine(info);
+            } catch (LineUnavailableException e) {
+            	throw new IllegalStateException(e);
+            }
+            
+            try {
+ 
+                if (line != null) {
+                    line.open(outFormat);
+                    line.start();
+                    stream(getAudioInputStream(outFormat, in), line);
+                    line.drain();
+                    line.stop();
+                }
+ 
+            } catch (LineUnavailableException 
+            		| IOException e) {
+            		throw new IllegalStateException(e);
+        	}
+        } catch (IOException | UnsupportedAudioFileException e1) {
+			e1.printStackTrace();
+		}
+    }
+ 
+    private AudioFormat getOutFormat(AudioFormat inFormat) {
+        final int ch = inFormat.getChannels();
+        final float rate = inFormat.getSampleRate();
+        return new AudioFormat(PCM_SIGNED, rate, 16, ch, ch * 2, rate, false);
+    }
+ 
+    private void stream(AudioInputStream in, SourceDataLine line) 
+        throws IOException {
+        final byte[] buffer = new byte[65536];
+        for (int n = 0; n != -1; n = in.read(buffer, 0, buffer.length)) {
+            line.write(buffer, 0, n);
+        }
+    }
 }
